92ea7d68c272936cd43fc368ad6a5c436e386a19
LeezaKhokhani <6755leezakhokhani@gmail.com>
2023-02-28 17:11:27 +0530

    cupertinoapp

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:cuoertino_app/screens/homeoage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
---
+++ lib/screens/global.dart
@@ -0,0 +1,61 @@
+class Global {
+  static List cartList = [];
+  static List<Map> productsList = [
+    {
+      "name": "Air Max 270 G",
+      "price": "30,000",
+      "image": "assets/images/air max 270.jpeg",
+    },
+    {
+      "name": "Nike Ultra Zoom",
+      "price": "15,000",
+      "image": "assets/images/Nike Ultra zoom.jpeg",
+    },
+    {
+      "name": "Nike Power Pro ",
+      "price": "18,000",
+      "image": "assets/images/Nike Power Pro.jpeg",
+    },
+    {
+      "name": "Nike Revolution 6",
+      "price": "5500",
+      "image": "assets/images/nike Revolution 6.jpeg",
+    },
+    {
+      "name": "Air Retro",
+      "price": "8,599",
+      "image": "assets/images/Air Retro.jpeg",
+    },
+    {
+      "name": "Nike Air Jorden ",
+      "price": "25,000",
+      "image": "assets/images/Nike Air joeden.jpeg",
+    },
+    {
+      "name": "Nike Air Max9",
+      "price": "12,000",
+      "image": "assets/images/Nike Air Max9.jpeg",
+    },
+    {
+      "name": "Air Force 1",
+      "price": "28,000",
+      "image": "assets/images/Air Force1.jpeg",
+    },
+    {
+      "name": "Nike Pegasus Trail3",
+      "price": "30,000",
+      "image": "assets/images/Nike Pegasus Trail3.jpeg",
+    },
+  ];
+}
+
+// titleText(String title) {
+//   return Container(
+//     padding: const EdgeInsets.all(12),
+//     alignment: Alignment.bottomLeft,
+//     child: Text(
+//       title,
+//       style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 34),
+//     ),
+//   );
+// }
